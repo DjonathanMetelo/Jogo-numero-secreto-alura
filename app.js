8ce0a624e28741ad508ba39ceb05b27e5fbb71c3
Djonathan Metelo <djonathanmetelo@gmail.com>
2026-02-24 16:09:22 -0400

    adicionando condicional para checar se chute bate com numero secreto e criando pasta para guardar atividades

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 alert('Boas-vindas ao jogo do número secreto'); //imprime um pop up de mensagem na tela;
 let numeroSecreto = 5; //cria uma variavel com o nome selecionado, e nesse caso atribui um valor a ela usando o simbulo "="
+console.log(numeroSecreto) //imprime oque está contido entre () no log da pagina
 let chute = prompt('Escolha um número entre 1 e 10'); //Pede ao usuario que ele insira um dado, seja ele uma string, tecla ou etc; dentro das ("") aparece o testo que será exibido ao usuario e nesse caso atribui o que o usuario digitou a uma variavel; nesse caso a variavel "chute"
 
 if (chute == numeroSecreto) { //condicional que caso ela seja verdadeira ela executara o codigo contido dentro dela
-        console.log("Isso aí! você descobriu o número secreto (5)"); //mostra uma mensagem no log da pagina
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`); //o uso de `` permite que alem do texto escrito entre `` seja tambem possivel imprimir a variavel contida de "${}"
+} else {
+    alert('Você errou :(')
 }
