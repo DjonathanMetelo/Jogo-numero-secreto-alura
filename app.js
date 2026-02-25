a8e982d38191b5341e0806e51daad2a5c5635c6d
Djonathan Metelo <djonathanmetelo@gmail.com>
2026-02-24 21:41:12 -0400

    avançando o projeto e incremento de atividades

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-let titulo = document.querySelector('h1'); //abre o documento HTML e seleciona a tag digitara entre "('')", nesse caso atribuimos essa tag aberta a variavel titulo
-titulo.innerHTML = 'Jogo do número secreto'; //insere na tag selecionada dentro da variavel o valor inserido apos o "="
+function exibirTextoNaTela(tag, texto) { //
+    let campo = document.querySelector(tag); //abre o documento HTML e seleciona a tag digitara entre "('')", nesse caso atribuimos essa tag aberta a variavel campo
+    campo.innerHTML = texto; //insere na tag selecionada dentro da variavel o valor inserido apos o "="
+};
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+function gerarNumeroAleatorio() {
+    parseInt(Math.random() * 10 + 1);
+};
 
-function verificarChute() { //cria e chama a respectiva função no documento html
-  console.log('O botão foi clicado!')
+function verificarChute() { //cria respectiva função no documento html
+  let chute = document.querySelector('input').value;//
+  console.log(chute == numeroSecreto);
 }
+
+let numeroSecreto = gerarNumeroAleatorio();
+exibirTextoNaTela('h1', 'Jogo do número secreto'); //
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
